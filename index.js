b67e41e4d432a05b06f193b90c2fcedfdf567e9f
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-09-25 21:22:36 +0300

    Task completed

---
+++ index.js
@@ -0,0 +1,59 @@
+const { Command } = require("commander");
+const {
+  listContacts,
+  addContact,
+  getContactById,
+  removeContact,
+} = require("./contacts");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts()
+        .then((contacts) => console.table(contacts))
+        .catch(console.error);
+      break;
+
+    case "get":
+      getContactById(id)
+        .then((contact) => {
+          if (contact) {
+            console.table(contact);
+          } else {
+            console.log("Contact not found");
+          }
+        })
+        .catch(console.error);
+      break;
+
+    case "add":
+      addContact(name, email, phone)
+        .then((contact) => console.table(contact))
+        .catch(console.error);
+      break;
+
+    case "remove":
+      removeContact(id)
+        .then((contacts) => console.table(contacts))
+        .catch(console.error);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
